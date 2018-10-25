0ba98b2abb11ceb5b9a28b38913177b000d6bff6
Nahid Fatma <30729630+nfatma@users.noreply.github.com>
2018-10-25 22:55:50 +0530

    Update leftRotate.test.js

--- src/leftRotate.test.js
+++ src/leftRotate.test.js
@@ -2,18 +2,18 @@ import { leftRotate } from './leftRotate';
 
 describe('leftRotate', () => {
   it('returns array roated to left by k unit place', () => {
-  expect(simpleCalculator([1,2,3,4,5,6,7], 3)).toEqual([4,5,6,7,1,2,3]);
+  expect(leftRotate([1,2,3,4,5,6,7], 3)).toEqual([4,5,6,7,1,2,3]);
   });
 
   it('returns array roated to left by k unit place', () => {
-  expect(simpleCalculator([1,2,3,4,5,6,7], 25)).toEqual([5,6,7,1,2,3,4]);
+  expect(leftRotate([1,2,3,4,5,6,7], 25)).toEqual([5,6,7,1,2,3,4]);
   });
 
   it('Invalid parameters', () => {
-  expect(simpleCalculator("a", 3)).tothrow();
+  expect(leftRotate("a", 3)).tothrow();
   });
 
   it('Invalid parameters', () => {
-  expect(simpleCalculator([1,2,3,4], "@")).tothrow();
+  expect(leftRotate([1,2,3,4], "@")).tothrow();
   });
 });
